717a05d82ca21e60d7ba77c4c551ef97b0c625b2
Fabricio Catae <fcatae@microsoft.com>
2017-09-07 20:32:46 -0300

    Add project files.

---
+++ ConsoleAppStor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConsoleAppStor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            
+            foreach (var key in appSettings.AllKeys)
+            {
+                string value = appSettings[key];
+
+                Console.WriteLine($"Setting [{key}] = '{value}'");
+            }
+        }
+    }
+}
