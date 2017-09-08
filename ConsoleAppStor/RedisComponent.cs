5972a77c306b819be5392332926952546a612b79
Fabricio Catae <fcatae@microsoft.com>
2017-09-08 17:50:37 -0300

    add redis

---
+++ ConsoleAppStor/RedisComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ConsoleAppStor
+{
+    class RedisComponent : ISource, ITarget
+    {
+        private string _connectionString;
+
+        public RedisComponent(string connectionString)
+        {
+            this._connectionString = connectionString;
+        }
+
+        public void Add(string key, string value)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(_connectionString);
+            IDatabase db = redis.GetDatabase();
+
+            db.StringSet(key, value);
+        }
+
+        public string Get(string key)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(_connectionString);
+            IDatabase db = redis.GetDatabase();
+
+            return db.StringGet(key);
+        }
+    }
+}
